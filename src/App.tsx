9d06e52695c6765ded0b2ea3e317852597a91bba
gothica <semidevilish666@gmail.com>
2022-12-08 17:51:48 +0300

    feat: setup gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header/header";
 
 import Buy from "./pages/buy/buy";
