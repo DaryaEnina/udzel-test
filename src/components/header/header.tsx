0636931f88b88224c6737bf679bffeef4ce78b9a
Mikhail Kolodenets <mishakolode@gmail.com>
2022-12-12 19:51:59 +0300

    feat: add header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,35 @@
 import { Link } from "react-router-dom";
 import "./style.scss";
+import logo from '../../assets/Logo.png'
 
 const Header = () => {
   return (
-    <ul className="nav">
-      <li>
-        <Link to="/buy">Купить</Link>
-      </li>
-      <li>
-        <Link to="/help">Помочь</Link>
-      </li>
-      <li>
-        <Link to="/project">О проекте</Link>
-      </li>
-    </ul>
+    <header>
+      <div className="container">
+        <div className="headerNav">
+          <Link to='/'>
+            <img src={logo} alt="logo"/>
+          </Link>
+          <nav>
+            <ul className="nav">
+              <li>
+                <Link to="/buy">Купить</Link>
+              </li>
+              <li>
+                <Link to="/help">Помочь</Link>
+              </li>
+              <li>
+                <Link to="/project">О проекте</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+          <button type="button" className="outlinedBtn">
+            <p className="gradientText">Войти</p>
+          </button>
+      </div>
+    </header>
   );
 };
 export default Header;
