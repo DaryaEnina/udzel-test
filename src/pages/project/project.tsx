6f2118a4bcb49d87a58f626226c0fa32b7cd3078
gothica <semidevilish666@gmail.com>
2022-12-14 16:26:14 +0300

    feat: project page

--- src/pages/project/project.tsx
+++ src/pages/project/project.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import girlImg from "../../assets/images/girl.png";
+import bag from "../../assets/images/bag.png";
+import coin from "../../assets/images/coin.png";
+import hands from "../../assets/images/hands.png";
+import Button from "../../components/button/button";
+import "./style.scss";
 
 const Project = () => {
-  return <h1>Project page</h1>;
+  return (
+    <main className="project_page-wrapper">
+      <div className="project_page-section">
+        <div className="project_page-section-text">
+          <p className="project_page-section-text-text">
+            Покупайте товары в любимых магазинах, делайте заказы в привычных
+            сервисах и получайте кешбек баллами, а в конце месяца распределите
+            их на помощь нуждающимся
+          </p>
+          <Button>
+            <p className="gradientText project_page-btn">К покупкам</p>
+          </Button>
+        </div>
+        <div className="project_page-section-image">
+          <img src={girlImg} alt="girl" />
+        </div>
+      </div>
+      <h2>Как работает благотворительный кешбэк?</h2>
+      <div className="project_page-images">
+        <img src={bag} alt="bag" />
+        <img src={coin} alt="coin" />
+        <img src={hands} alt="hands" />
+      </div>
+    </main>
+  );
 };
 
 export default Project;
